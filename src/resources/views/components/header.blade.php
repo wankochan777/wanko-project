e45af7cc0639099b0669a5a41c6f3e96fc90d5ea
wankochan777 <shindame@hotmail.com>
2022-03-30 00:37:48 +0900

    ヘッダー、フッター作成

---
+++ src/resources/views/components/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="project-title">
+        <a>My Movie Review</a>
+    </div>
+
+    <div class="project-logo">
+        <img src="image/movie_film_logo.png">
+    </div>
+
+
+
+</header>
