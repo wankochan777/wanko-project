d140758bff45ffaf7998341f1bc12779a6d6e8fb
wankochan777 <shindame@hotmail.com>
2022-04-06 12:46:25 +0900

    css読み込み方法変更

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -25,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if(\App::environment('local')) {
+            \URL::forceScheme('https');
+        }
     }
 }
 
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -11,7 +11,11 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link href="{{ secure_asset('assets/css/app.css') }}" rel="stylesheet">
+        @if(app('env') == 'production')
+            <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+        @else
+            <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        @endif
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
